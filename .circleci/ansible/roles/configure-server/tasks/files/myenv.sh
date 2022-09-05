4c8f1dec6afddea1bfcdeb3a9e29b78a71187e1f
Kisha-net <antoinette.akinyi@azubiafrica.org>
2022-09-05 15:55:56 +0300

    working on configure infrastructure

---
+++ .circleci/ansible/roles/configure-server/tasks/files/myenv.sh
@@ -0,0 +1,9 @@
+export TYPEORM_CONNECTION=postgres
+export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
+export TYPEORM_HOST=postgres.cbom5ios6zvj.us-east-1.rds.amazonaws.com
+export TYPEORM_PORT=5432
+export TYPEORM_USERNAME=postgres
+export TYPEORM_PASSWORD=postgres
+export TYPEORM_DATABASE=postgres
+export TYPEORM_MIGRATIONS=./src/migrations/*.ts
+export TYPEORM_MIGRATIONS_DIR=./src/migrations
